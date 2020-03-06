8a2fd9bf84775cd3f7e888aa0cdeba5a7934cc99
jborza <jborza@gmail.com>
2020-03-06 21:33:37 +0100

    search prototype

---
+++ search.py
@@ -0,0 +1,33 @@
+import json
+import fileinput
+import re
+
+search = {'_source': {'excludes': ['geo', 'message', 'tags', 'event', 'phpmemory']}, 
+'query': {'bool': {'must': [], 
+'filter': [{'match_all': {}}, {'match_phrase': {'host': 'www.elastic.co'}}, {'match_phrase': {'geo.dest': 'CN'}}, {'match_phrase': {'agent': 'Firefox'}}, {'bool': {'should': [{'match_phrase': {'machine.os': 'osx'}}, {'match_phrase': {'machine.os': 'ios'}}], 'minimum_should_match': 1}}], 'must_not': [{'match_phrase': {'response': 200}}]}}}
+
+simple_search = {}
+
+#operate on a single file
+filename = 'logs_dataset_sorted.json'
+
+def match(json, search):
+    if not (re.search('www.elastic.co', json['host'])):
+        return False
+    if not (re.search('Firefox',json['agent'])):
+        return False
+    if not (json['machine']['os'] in ['ios', 'osx']):
+        return False
+    if (json['geo']['dest'] != 'CN'):
+        return False
+    if(json['response'] == 200):
+        return False
+    return True
+
+with open(filename,'r') as file_input:
+    #for line in fileinput.input():
+    for line in file_input:
+        json_data = json.loads(line)
+        if(match(json_data, search)):
+            print(json.dumps(json_data))
+
